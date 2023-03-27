c267198eb9f2d3cd086321f7b0e9bb87c04e7190
emhamza <imumairhamza@gmail.com>
2023-03-28 00:08:51 +0500

    webpack setup completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import _ from 'lodash';
+import './style.css';
+import printMe from './print.js';
+import Icon from './icon.png';
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
   
   // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+  
   
     return element;
   }
